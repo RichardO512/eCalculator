7e31def036cfc2bc935dd538028788b308c9886d
Richard Ombrog <ombrog512@gmail.com>
2021-11-10 17:30:52 +0800

    basic calculator

---
+++ index.js
@@ -0,0 +1,9 @@
+function calcNumbers(result){
+    form.displayResult.value=form.displayResult.value+result;
+    
+    // if(result == "acB") {
+    //     // All Clear
+    //     inputLabel.innerHTML = clear();
+    // }
+
+  }
